dfd1202b7a8138885f44ee69589838b76d7e154a
José Miguel Alvarez <j.m.alvarez.camacho@gmail.com>
2026-03-28 09:51:17 -0400

    chore: agregado compose

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
-docker run -d -p 80:8080 --name inventory-"$hostname" jmdiplomado/pruebagitflow2026:"$hostname"
+echo "services:
+  postgres:
+    image: postgres:16
+    container_name: postgres-diplomado-2
+    environment:
+      POSTGRES_PASSWORD: postgresspassword
+      POSTGRES_USER: postgres
+      POSTGRES_DB: mydb
+    ports:
+      - "5432:5432"
+    volumes:
+      - postgres_data:/var/lib/postgresql/data
+  api:
+    image: jmdiplomado/pruebagitflow2026:$hostname
+    container_name: inventory-api
+    ports:
+      - "80:8080"
+    depends_on:
+      - postgres
+    environment:
+      ASPNETCORE_ENVIRONMENT: Development
+      ConnectionStrings__InventoryDatabase: Host=postgres;Port=5432;Database=StoreInventory;Username=postgres;Password=postgresspassword;Include Error Detail=true
+
+volumes:
+  postgres_data:
+" > compose.yml
+docker-compose up -d
